e326d69ec7d1b01222cb4626d5f9523454633fee
toomm17 <mihail.karpinskii@mail.ru>
2023-05-24 22:29:37 +0300

    Add postgres_cursor fixture

--- conftest.py
+++ conftest.py
@@ -1,32 +1,33 @@
 import pytest
+import psycopg2
 
-import _types
+import db_config
 import utils
 
 
-# @pytest.fixture()
-# def postgres():
-#     """
-#         Фикстура для получения коннекта к постгресу
-#         После завершения теста коннект закрывается
-#     """
-#     postgres_credentials = _types.DatabaseCredentials(
-#         host=config.POSTGRES_HOST,
-#         port=config.POSTGRES_PORT,
-#         user=config.POSTGRES_USER,
-#         password=config.POSTGRES_PASSWORD,
-#         dbname=config.POSTGRES_DB_NAME
-#     )
-
-#     postgres_connection = utils.create_postgres_connection(postgres_credentials)
-#     yield postgres_connection
-#     postgres_connection.close()
+@pytest.fixture()
+def postgres_cursor():
+    """
+        Фикстура для получения коннекта к постгресу
+        После завершения теста коннект закрывается
+    """
+    conn = psycopg2.connect(
+        dbname=db_config.POSTGRES_DB_NAME,
+        user=db_config.POSTGRES_USER,
+        password=db_config.POSTGRES_PASSWORD,
+        host=db_config.POSTGRES_HOST,
+        port=db_config.POSTGRES_PORT
+    )
+    cursor = conn.cursor()
+    yield cursor
+    cursor.close()
+    conn.close()
 
 
 @pytest.fixture()
 def repl_thr_list():
     """
-        Получаем из config.json все имена потоков
+        Получаем из config.json все потоки
         :return: list[string]
     """
     json_data = utils.get_json_data()
@@ -40,18 +41,22 @@ def multistart_repl_thr():
         :return: list[string]
     """
     json_data = utils.get_json_data()
-    threads_list = json_data['threads']['list']
-    num_threads = json_data['threads']['maxRunning']
-    return threads_list[0:num_threads]
+    threads_list = [
+        thread for thread in json_data['threads']['list'] 
+        if thread['is_single_run'] is False
+    ]
+    return threads_list
 
 
 @pytest.fixture()
 def singlestart_repl_thr():
     """
-        Получаем из config.json все потоки для многопоточного запуска
+        Получаем из config.json все потоки для однопоточного запуска
         :return: list[string]
     """
     json_data = utils.get_json_data()
-    threads_list = json_data['threads']['list']
-    num_threads = json_data['threads']['maxRunning']
-    return threads_list[num_threads:]
+    threads_list = [
+        thread for thread in json_data['threads']['list'] 
+        if thread['is_single_run'] is True
+    ]
+    return threads_list
